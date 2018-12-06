951062382c4fb25bfc101b3c8fc90079b1ba684d
VIPsth <VIPsongtaohong@yeah.net>
2018-12-06 12:12:36 +0800

    add

--- static/js/custom.js
+++ static/js/custom.js
@@ -9,6 +9,18 @@
 		clearTimeout(this.responseTimer);
 		this.responseTimer = setTimeout(response, 300);
 	};
+	
+	function inputblur(){
+		$('input').on('focus',function(e){
+			this.scrollIntoView();
+			document.getElementsByTagName('body')[0].scrollTop = document.getElementsByTagName('body')[0].scrollHeight;
+			document.getElementsByTagName('html')[0].scrollTop = document.getElementsByTagName('html')[0].scrollHeight;
+		});
+		$('input').on('blur',function( ){
+			$('body').scrollTop(0);
+		});
+	}
 $(function(){
 	response();
+	inputblur();
 })
