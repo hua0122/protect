cc4ba3a55e2d024d453855a378c9645e6f1cad58
VIPsth <VIPsongtaohong@yeah.net>
2019-01-04 15:02:30 +0800

    add

--- static/js/api.js
+++ static/js/api.js
@@ -110,7 +110,7 @@ function select_student(ajaxdata) {
 		}
 	}
 	if (data.status == "500") {
-		mui.alert("您查询的学员已经被保护\n\n保护时间：<font>" + data.data.protect_time + "</font>\n脱保时间：<font>" + data.data.protect_time +
+		mui.alert("您查询的学员已经被保护\n\n保护时间：<font>" + data.data.protect_time + "</font>\n脱保时间：<font>" + data.data.deactivation_time +
 			"</font>", " ")
 	}
 }
